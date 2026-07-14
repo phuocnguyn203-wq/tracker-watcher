adde4f70cc852e658e6426adfc6b3b182e340888
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-07-14 10:53:00 +0700

    add create user service

---
+++ app/routers/users.py
@@ -0,0 +1,51 @@
+from typing import Annotated
+from app.config import settings
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from app.schemas.users import Token, User
+
+from datetime import timedelta
+from app.dependencies import get_current_user_dep, get_current_user_dep
+from app.services import users as users_service
+
+router = APIRouter(
+    prefix='/users',
+    tags=['users']
+)
+
+@router.post('/token')
+async def login_for_access_token(
+    db: Annotated[Session, get_current_user_dep],
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = users_service.authenticate_user(
+        db,
+        form_data.username,
+        form_data.password
+    )
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='username or password is wrong',
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
+    
+    access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+    access_token = users_service.create_access_token(
+        data={'sub': user.username},
+        expires_delta=access_token_expires,
+    )
+    
+    return Token(
+        access_token=access_token,
+        token_type='bearer'
+    )
+
+
+@router.get('/me')
+async def read_user_me(
+    user: Annotated[User, get_current_user_dep],
+):
+    return user
