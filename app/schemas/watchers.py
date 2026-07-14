7ea9b8772a5dd38f66c80de4d5648e1b1bd22512
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-07-15 00:27:24 +0700

    add ReturnedWatcher to return in routes

--- app/schemas/watchers.py
+++ app/schemas/watchers.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 class CreateWatcher(BaseModel):
     name: str
@@ -11,4 +12,22 @@ class CreateWatcher(BaseModel):
     
     interval_minutes: int
     last_state: bool = Field(default=False)
+
+class ReturnedWatcher(BaseModel):
+    id: int
+    
+    user_id: int
+    
+    name: str
+    
+    source_type: str
+    product_id: str
+    
+    cc: str
+    target_price: int
+    
+    interval_minutes: int
+    last_state: bool = Field(default=False)
+    last_price: int | None = None
+    last_checked_at: datetime | None = None
     
