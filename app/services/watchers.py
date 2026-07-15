62d0b518ace5b5aea477f0f1290a09afe3f3304c
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-07-15 10:53:20 +0700

    add get_specific_watcher_by_id function in watchers services

--- app/services/watchers.py
+++ app/services/watchers.py
@@ -1,3 +1,5 @@
+from collections.abc import Sequence
+
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
@@ -27,8 +29,23 @@ def create_watcher(
 def get_all_watchers_for_users(
     db: Session,
     user_id: int,
-):
+) -> Sequence[Watcher]:
     stmt = select(Watcher).where(Watcher.user_id==user_id)
     all_watchers = db.scalars(stmt).all()
     
     return all_watchers
+
+def get_specific_watcher(
+    db: Session,
+    user_id: int,
+    watcher_id: int,
+) -> Watcher | None:
+    stmt = (
+        select(Watcher).
+        where(Watcher.user_id==user_id).
+        where(Watcher.id==watcher_id)
+    )
+    
+    watcher = db.scalar(stmt)
+    return watcher
+    
