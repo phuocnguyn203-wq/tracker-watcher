4b166a9fe7caf1e150a2580b98a6881e014bd55d
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-07-16 12:18:06 +0700

    add should_notify function to check whether it should notify or not

---
+++ app/services/alerts/helper_functions.py
@@ -0,0 +1,11 @@
+from app.models.models import Watcher
+from app.services.fetcher.base import PriceResult
+
+def should_notify(
+    watcher: Watcher,
+    price_result: PriceResult
+):
+    if watcher.target_price < price_result.price:
+        if not watcher.last_state:
+            return True
+    return False
